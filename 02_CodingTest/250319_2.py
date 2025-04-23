9ffd4bcfb074ceb27ec3058e2bb0126041a13100
jylee9018 <jylee9018@gmail.com>
2025-04-23 23:01:53 +0900

    Update TIL

---
+++ 02_CodingTest/250319_2.py
@@ -0,0 +1,16 @@
+# import sys
+
+# answer = []
+# for _ in range(100):
+#     s = sys.stdin.readline()
+#     if s == '\n':
+#         break
+#     answer.append(s)
+    
+# for e in answer:
+#     print(e, end="")
+
+import sys
+data = sys.stdin.read()  # 전체 입력을 한 번에 가져오기
+print(data)
+    
